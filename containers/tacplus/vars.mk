959fb95b0062c0d0e8578ec4f8564b157a9b60fb
Ralph Schmieder <rschmied@cisco.com>
2026-02-06 12:41:43 +0100

    no date for tacplus

--- containers/tacplus/vars.mk
+++ containers/tacplus/vars.mk
@@ -1,5 +1,6 @@
 # the library versioning does not change, use date-based versioning
-VERSION      := F4.0.4.28-$(shell date -u +%Y%m%d)
+# VERSION      := F4.0.4.28-$(shell date -u +%Y%m%d)
+VERSION      := F4.0.4.28
 NAME         := tacplus
 DESC         := Tacacs plus
 FULLDESC     := $(DESC) $(VERSION)
